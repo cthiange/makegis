93085b4f7de748384d016ebf639ea451c71328c8
chris <cthiange@users.noreply.github.com>
2025-09-30 14:56:52 +1300

    handle srid and indexing of duckdb tables

--- src/makegis/dag.py
+++ src/makegis/dag.py
@@ -5,6 +5,7 @@ import subprocess
 from typing import Dict
 from typing import List
 from typing import Set
+from typing import Tuple
 
 from pydantic import BaseModel
 import yaml
@@ -36,8 +37,17 @@ class DuckDBSource(BaseModel):
     table: str
 
 
+class NodeDefaults(BaseModel):
+    geom_index: bool = False
+    src_epsg: int | None = None
+    dst_epsg: int | None = None
+
+
 class Table(BaseModel):
     name: str
+    geom_index: bool
+    src_epsg: int | None
+    dst_epsg: int | None
     src: SQLSource | DuckDBSource | FileSource | WFSSource
 
 
@@ -105,7 +115,6 @@ class DAG:
                 continue
             print(f"error - prep {i}/{n} {action} failed")
             raise RuntimeError("prep step failed")
-        return
         for table in nc.tables:
             load_table(target, table)
 
@@ -114,11 +123,15 @@ def parse_makegis_yml(path: Path, schema: str, prefix: str) -> NodeConfig:
     with open(path) as f:
         d = yaml.load(f, Loader)
     expand_dict_strings(d)
+
+    # defaults
+    node_defaults = parse_node_defaults(d)
+
     deps = []
     if "deps" in d:
         deps = d["deps"]
     prep = []
-    if "prep" in d:
+    if "prep" in d and d["prep"] is not None:
         prep = [Path(path.parent) / Path(item) for item in d["prep"]]
     tables = []
     for local_name, kvs in d["tables"].items():
@@ -137,7 +150,17 @@ def parse_makegis_yml(path: Path, schema: str, prefix: str) -> NodeConfig:
         else:
             raise RuntimeError(f"unknown source type in {path}")
         table_name = f"{schema}.{prefix}{'_' if prefix else ''}{local_name}"
-        table = Table(name=table_name, src=src)
+        if "epsg" in kvs:
+            src_epsg, dst_epsg = parse_epsg(kvs["epsg"])
+        else:
+            src_epsg, dst_epsg = node_defaults.src_epsg, node_defaults.dst_epsg
+        table = Table(
+            name=table_name,
+            src=src,
+            geom_index=kvs.get("index", node_defaults.geom_index),
+            src_epsg=src_epsg,
+            dst_epsg=dst_epsg,
+        )
         tables.append(table)
 
     return NodeConfig(
@@ -148,18 +171,55 @@ def parse_makegis_yml(path: Path, schema: str, prefix: str) -> NodeConfig:
     )
 
 
+def parse_epsg(e: int | str | None) -> Tuple[int | None, int | None]:
+    """Return (src, dst) EPSG's from `int` or `int:int` type string"""
+    if e is None:
+        return (None, None)
+    if isinstance(e, int):
+        return (None, e)
+
+    parts = e.split(":")
+    assert len(parts) == 2
+    return (int(parts[0]), int(parts[1]))
+
+
+def parse_node_defaults(d: Dict) -> NodeDefaults:
+    if "defaults" not in d or d["defaults"] is None:
+        return NodeDefaults()
+    dd = d["defaults"]
+    defs = {}
+    if "epsg" in dd:
+        defs["src_epsg"], defs["dst_epsg"] = parse_epsg(dd["epsg"])
+    if "index" in dd:
+        defs["geom_index"] = dd["index"]
+
+    return NodeDefaults(**defs)
+
+
 def load_table(target: Target, table: Table):
     if isinstance(table.src, DuckDBSource):
-        load_duckdb_table(target, table.name, table.src)
+        load_duckdb_table(target, table)
     elif isinstance(table.src, SQLSource):
         load_sql_source(target, table.name, table.src)
     else:
         raise NotImplementedError(f"table source not supported yet: f{table.src}")
 
 
-def load_duckdb_table(target: Target, table_name: str, src: DuckDBSource):
-    print(f"loading duckdb table {table_name}")
-    postgis.ddb2pg(src.path, target.conn_str(), src.table, table_name)
+def load_duckdb_table(target: Target, table: Table):
+    print(f"loading duckdb table {table.name}")
+    assert isinstance(table.src, DuckDBSource)
+    src = table.src
+    src_table = src.table
+    dst_table = table.name
+    postgis.ddb2pg(
+        src.path,
+        target.conn_str(),
+        src_table,
+        dst_table,
+        src_epsg=table.src_epsg,
+        dst_epsg=table.dst_epsg,
+        geom_index=table.geom_index,
+    )
 
 
 def load_sql_source(target: Target, table_name: str, src: SQLSource):
--- src/makegis/postgis.py
+++ src/makegis/postgis.py
@@ -1,12 +1,45 @@
 from pathlib import Path
+from typing import Tuple
 
 import duckdb
 import psycopg
 from psycopg import sql
 
 
+class Table:
+
+    def __init__(self, schema: str, name: str):
+        self._schema = schema
+        self._name = name
+        self.ident = sql.Identifier(schema, name)
+        self.literal_schema = sql.Literal(schema)
+        self.literal_table = sql.Literal(name)
+
+    def __str__(self):
+        return f"{self._schema}.{self._name}"
+
+
+class Column:
+
+    def __init__(self, name: str):
+        self._name = name
+        self.ident = sql.Identifier(name)
+        self.literal = sql.Literal(name)
+
+    def __str__(self):
+        return self._name
+
+
 # Helper function to load a single table from DuckDB to Postgres
-def ddb2pg(ddb_path: Path, conn_str: str, src_table, dst_table):
+def ddb2pg(
+    ddb_path: Path,
+    conn_str: str,
+    src_table,
+    dst_table,
+    src_epsg: int | None = None,
+    dst_epsg: int | None = None,
+    geom_index: bool = False,
+):
     print(f"postgis - loading duckdb table from {ddb_path}:{src_table} to {dst_table}")
     db = duckdb.connect()
     db.sql("install spatial;")
@@ -37,33 +70,33 @@ def ddb2pg(ddb_path: Path, conn_str: str, src_table, dst_table):
 
     # Add constraints to new postgres table
     with psycopg.connect(conn_str) as conn:
-        table = schema_name_identifier(dst_table)
+        table = Table(*split_schema_table_name(dst_table))
+
         for i, col_str, dtype, not_null, default, pk in columns:
-            col = sql.Identifier(col_str)
+            col = Column(col_str)
             if not_null:
                 conn.execute(
                     sql.SQL(
                         "alter table {table} alter column {col} set not null"
-                    ).format(table=table, col=col)
+                    ).format(table=table.ident, col=col.ident)
                 )
             if default:
                 conn.execute(
                     sql.SQL(
                         "alter table {table} alter column {col} set default {default}"
-                    ).format(table=table, col=col, default=sql.Literal(default))
+                    ).format(
+                        table=table.ident, col=col.ident, default=sql.Literal(default)
+                    )
                 )
             # Process geo columns
             if dtype == "GEOMETRY":
-                # logger.info(f"processing geometry column '{col}'")
-                conn.execute(
-                    sql.SQL(
-                        "alter table {table} alter column {col} type geometry using {col}::geometry;"
-                    ).format(table=table, col=col)
-                )
-                conn.execute(
-                    sql.SQL(
-                        "update {table} set {col} = st_setsrid({col}, 4326);"
-                    ).format(table=table, col=col)
+                _process_duckdb_geo_column(
+                    conn,
+                    table,
+                    col,
+                    src_epsg=src_epsg,
+                    dst_epsg=dst_epsg,
+                    geom_index=geom_index,
                 )
 
         # Add primary key if any
@@ -78,9 +111,157 @@ def ddb2pg(ddb_path: Path, conn_str: str, src_table, dst_table):
         conn.commit()
 
 
+def _process_duckdb_geo_column(
+    conn: psycopg.Connection,
+    table: Table,
+    col: Column,
+    src_epsg: int | None,
+    dst_epsg: int | None,
+    geom_index: bool = False,
+):
+    print(f"processing geometry column '{col}' for table '{table}'")
+    print(
+        f"trace - src_epsg: {src_epsg}, dst_epsg: {dst_epsg}, geom_index: {geom_index}"
+    )
+    if dst_epsg is None:
+        # Because epsg setting is either dst or src:dst, src cannot be set without dst
+        assert src_epsg is None, "src_epsg cannot be set without a dst_epsg"
+        # No epsg settings, just cast to generic geom
+        _duckdb_cast_geom_without_srid(conn, table, col)
+    if dst_epsg is not None:
+        if src_epsg is None or src_epsg == dst_epsg:
+            # Just set srid dst_epsg
+            _duckdb_cast_geom_with_srid(conn, table, col, dst_epsg)
+        else:
+            # Set srid and transform
+            _duckdb_cast_geom_with_transform(conn, table, col, src_epsg, dst_epsg)
+
+    if geom_index:
+        _ensure_gist_index(conn, table, col)
+
+
+def _duckdb_cast_geom_without_srid(
+    conn: psycopg.Connection,
+    table: Table,
+    col: Column,
+):
+    """Casts the geo column to a generic geometry type"""
+    print("trace - _duckdb_cast_geom_without_srid")
+    conn.execute(
+        sql.SQL(
+            "alter table {table} alter column {col} type geometry using {col}::geometry"
+        ).format(table=table.ident, col=col.ident)
+    )
+
+
+def _duckdb_cast_geom_with_srid(
+    conn: psycopg.Connection,
+    table: Table,
+    col: Column,
+    epsg: int,
+):
+    """Casts the geo column to a geometry type with srid"""
+    print("trace - _duckdb_cast_geom_with_srid")
+    conn.execute(
+        sql.SQL(
+            """
+            alter table {table}
+                alter column {col} type geometry(GEOMETRY, {epsg})
+                using {col}::geometry(GEOMETRY, {epsg})
+            """
+        ).format(table=table.ident, col=col.ident, epsg=sql.Literal(epsg))
+    )
+
+
+def _duckdb_cast_geom_with_transform(
+    conn: psycopg.Connection,
+    table: Table,
+    col: Column,
+    src_epsg: int,
+    dst_epsg: int,
+):
+    """Casts the geo column to a geometry type while transforming between srids"""
+    print("trace - _duckdb_cast_geom_with_transform")
+    conn.execute(
+        sql.SQL(
+            """
+            alter table {table}
+                alter column {col} type geometry(GEOMETRY, {dst_epsg})
+                using st_transform({col}::geometry(GEOMETRY, {src_epsg}), {dst_epsg})
+            """
+        ).format(
+            table=table.ident,
+            col=col.ident,
+            src_epsg=sql.Literal(src_epsg),
+            dst_epsg=sql.Literal(dst_epsg),
+        )
+    )
+
+
+def _ensure_gist_index(conn: psycopg.Connection, table: Table, col: Column):
+    if _column_has_index(conn, table, col, "gist"):
+        return
+
+    print(f"debug - creating index on {col} in {table}")
+    conn.execute(
+        sql.SQL(
+            """
+            create index on {table} using GIST({col});
+            """
+        ).format(table=table.ident, col=col.ident)
+    )
+
+
 # TODO: refactor to not need this anymore (i.e. use distinct schema/table args)
 def schema_name_identifier(sn: str) -> sql.Identifier:
-    if "." in sn:
-        schema = sn.split(".")[0]
-    table = sn[len(schema) + 1 :]
+    schema, table = split_schema_table_name(sn)
     return sql.Identifier(schema, table)
+
+
+def split_schema_table_name(sn: str) -> Tuple[str, str]:
+    """
+    Split a 'schema.table_name type string into (schema, table_name)
+
+    Assumes schema to always be present.
+    """
+    assert "." in sn
+    schema = sn.split(".")[0]
+    table = sn[len(schema) + 1 :]
+    return (schema, table)
+
+
+def _column_has_index(
+    conn: psycopg.Connection,
+    table: Table,
+    col: Column,
+    index_type: str,
+) -> bool:
+    qry = sql.SQL(
+        """
+        select
+            c.relname as index_name,
+            x.indisunique as is_unique,
+            x.indisprimary as is_primary,
+            x.indisvalid as is_valid,
+            x.indisready as is_ready,
+            am.amname as index_type,
+            a.attname as column_name
+        from pg_class t
+        join pg_namespace n on n.oid = t.relnamespace
+        join pg_index x on t.oid = x.indrelid
+        join pg_class c on c.oid = x.indexrelid
+        join pg_am am on c.relam = am.oid
+        join pg_attribute a on a.attrelid = t.oid and a.attnum = any(x.indkey)
+        where n.nspname = {schema}
+            and t.relname = {table}
+            and a.attname = {column}
+            and am.amname ilike {index_type};
+    """
+    ).format(
+        schema=table.literal_schema,
+        table=table.literal_table,
+        column=col.literal,
+        index_type=sql.Literal(index_type),
+    )
+    rows = conn.execute(qry).fetchall()
+    return len(rows) > 0
