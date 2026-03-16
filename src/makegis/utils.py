b2edc13055b557354449e274266a360d51eacdb3
chris <cthiange@users.noreply.github.com>
2026-03-16 15:03:01 +1300

    fix: ensure python action tracebacks are logged as errors

--- src/makegis/utils.py
+++ src/makegis/utils.py
@@ -4,13 +4,19 @@ log = logging.getLogger("makegis")
 
 
 def capture_logs(stream, log_prefix: str):
+    # Flag to indicate a traceback was detected in the logs
+    traceback = False
+
     if stream is not None:
         for line in stream:
             msg = f"[{log_prefix}] {line.strip()}"
             lower_line = line.lower()
-            if "error" in line.lower():
+            if traceback or "error" in line.lower():
                 log.error(msg)
             elif "warn" in lower_line:
                 log.warning(msg)
+            elif "traceback (most recent call last)" in lower_line:
+                traceback = True
+                log.error(msg)
             else:
                 log.info(msg)
